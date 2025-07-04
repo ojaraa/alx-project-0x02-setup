a38e90b609b0d1fc72e9ecf92ec48f18ca4ec8c2
ojara <ojarafavour@yahoo.com>
2025-07-04 15:11:13 +0100

    added inteface and images

--- alx-project-0x02/components/layout/Header.tsx
+++ alx-project-0x02/components/layout/Header.tsx
@@ -1,19 +1,19 @@
-import Link from 'next/link'
-import React from 'react'
+import Link from "next/link";
+import React from "react";
 
 const Header = () => {
   return (
-    <div className='flex items-center justify-between px-6 py-4'>
-   <h1>     Header</h1>
+    <div className="flex items-center justify-between px-6 py-4">
+      <h1> Header</h1>
 
-        <nav>
-            <ul className='flex gap-x-5'>
-                <Link href={'home'}>Home</Link>
-                <Link href={'about'}>About</Link>
-            </ul>
-        </nav>
+      <nav>
+        <ul className="flex gap-x-5">
+          <Link href={"home"}>Home</Link>
+          <Link href={"about"}>About</Link>
+        </ul>
+      </nav>
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
---
+++ alx-project-0x02/interfaces/index.ts
@@ -0,0 +1,7 @@
+export interface CardProps {
+  title: string;
+  description: string;
+  imageUrl?: string;
+  link?: string;
+  tags: string[];
+}
