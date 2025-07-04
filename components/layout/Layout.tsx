3f4f6c12df0b2173993aea94274fb002f33928f8
ojara <ojarafavour@yahoo.com>
2025-07-04 14:59:32 +0100

    added layout and header

---
+++ components/layout/Layout.tsx
@@ -0,0 +1,19 @@
+import Header from "./Header";
+// import Footer from "./Footer";
+import React from "react";
+
+interface LayoutProps {
+  children: React.ReactNode;
+}
+
+const Layout: React.FC<LayoutProps> = ({ children }) => {
+  return (
+    <>
+      <Header />
+      <main className="min-h-screen">{children}</main>
+      {/* <Footer /> */}
+    </>
+  );
+}
+
+export default Layout;
